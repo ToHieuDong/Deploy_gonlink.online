e7cfffec325fb6eb0bfc7d614b5166ac83322a61
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2024-11-04 18:00:32 +0700

    fix mobile

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -13,27 +13,48 @@ export default function Login() {
   }
 
   return (
-    <div className='h-screen overflow-hidden flex'>
-        <div className='w-[30%] bg-slate-400'>
+    <div>
+      <div className="hidden md:block">
+        <div className='h-screen overflow-hidden flex'>
+          <div className='w-[30%] bg-slate-400'>
 
-        </div>
-        <div className='w-[70%] my-72'>
+          </div>
+          <div className='w-[70%] my-72'>
             <div className='flex justify-center'>
-                <div>
-                    <h3 className='font-bold text-xl p-2'>Login using a social network</h3>
-                    
-                    <button onClick={handleLogin} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGithub} /> Sign in with Github</button>
-                    <button onClick={handleLoginGG} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGoogle} /> Sign in with Google</button>
-
-                    <p className=''>Bằng việc bấm vào nút Đăng nhập ở trên, có nghĩa là bạn đã</p>
-                    <p className=''>đồng ý với <span className='font-bold text-sky-600 mx-1'>Chính sách và điều khoản sử dụng</span> của chúng tôi.</p>
-                </div>
+              <div>
+                <h3 className='font-bold text-xl p-2'>Login using a social network</h3>
                 
-            </div>
+                <button onClick={handleLogin} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGithub} /> Sign in with Github</button>
+                <button onClick={handleLoginGG} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGoogle} /> Sign in with Google</button>
 
-            
-            
+                <p className=''>Bằng việc bấm vào nút Đăng nhập ở trên, có nghĩa là bạn đã</p>
+                <p className=''>đồng ý với <span className='font-bold text-sky-600 mx-1'>Chính sách và điều khoản sử dụng</span> của chúng tôi.</p>
+              </div>
+                
+            </div> 
+          </div>
         </div>
+      </div>
+
+      <div className="block md:hidden">
+        <div className='h-screen overflow-hidden flex bg-gradient-to-b from-white to-blue-200'>
+          <div className='my-60 mx-10'>
+            <div className='flex justify-center'>
+              <div>
+                <h3 className='font-bold text-xl p-2'>Login using a social network</h3>
+                
+                <button onClick={handleLogin} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGithub} /> Sign in with Github</button>
+                <button onClick={handleLoginGG} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGoogle} /> Sign in with Google</button>
+
+                <p className=''>Bằng việc bấm vào nút Đăng nhập ở trên, có nghĩa là bạn đã</p>
+                <p className=''>đồng ý với <span className='font-bold text-sky-600 mx-1'>Chính sách và điều khoản sử dụng</span> của chúng tôi.</p>
+              </div>
+                
+            </div> 
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
