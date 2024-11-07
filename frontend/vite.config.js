b154ab4fc63d2309a6b18ad624f0558cbe22b6d2
sonnv <sonnv@unit.com.vn>
2024-11-07 23:12:55 +0700

    change domain backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,19 @@ import react from '@vitejs/plugin-react'
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     global: {},
     'process.env': process.env
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://www.gonlink.online', // Địa chỉ của backend server
+        changeOrigin: true,
+        secure: true // Đảm bảo proxy sử dụng HTTPS
+      }
+    }
+  }
 })
